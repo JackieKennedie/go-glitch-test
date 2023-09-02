7cc964adc506932e780137648dfa59544f8d300c
JackieKennedie <stwhetstone@gmail.com>
2023-09-01 23:54:44 -0400

    add get

--- main.go
+++ main.go
@@ -6,15 +6,20 @@ import (
 	"os"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "<html><body><button>buton</button></body><html>")
-}
-
 func main() {
 	listenPort := ":8080"
 	if val := os.Getenv("PORT"); val != "" {
 		listenPort = ":" + val
 	}
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/get", get)
 	http.ListenAndServe(listenPort, nil)
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "<html><body><button>buton</button></body><html>")
+}
+
+func get(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, 123)
 }
